e7842f875d582ae82e63e7e7a64128dc453e4050
Uriel Gonzalez <gonzalezcasianouriel@gmail.com>
2026-04-21 02:57:31 -0600

    Update __init__.py

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -15,9 +15,11 @@ def create_app() -> Flask:
 
     from routes.director    import director_bp
     from routes.supervisor  import supervisor_bp
+    from routes.secretaria  import secretaria_bp
 
     app.register_blueprint(director_bp)
-    app.register_blueprint(supervisor_bp)
+    app.register_blueprint(supervisor_bp)\
+    app.register_blueprint(secretaria_bp)
 
 
     @app.route("/api/health")
