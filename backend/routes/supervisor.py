66a3aa7db4e935449d5a72ecd56493fc3145e385
Uriel Gonzalez <gonzalezcasianouriel@gmail.com>
2026-05-02 05:13:09 -0600

    Update supervisor.py

--- backend/routes/supervisor.py
+++ backend/routes/supervisor.py
@@ -2,6 +2,7 @@ from flask import Blueprint, request
 from app.database import get_db
 from app.helpers import ok, created, bad_request, not_found, db_error_response, require_fields
 import time
+from .auth import require_auth
 
 supervisor_bp = Blueprint("supervisor", __name__)
 
