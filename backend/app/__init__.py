ef561b62535e9599c6af89464e345fafba2d14e8
Uriel Gonzalez <gonzalezcasianouriel@gmail.com>
2026-05-02 08:08:18 -0600

    Update __init__.py

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -28,48 +28,12 @@ def create_app() -> Flask:
     app.register_blueprint(supervisor_bp)
     app.register_blueprint(secretaria_bp)
 
-    
-
-    @app.route("/auth/login", methods=["POST"])
-    def login():
-        try:
-            data = request.get_json()
-            username = data.get("username")
-            password_hash = data.get("password_hash")
-            rol = data.get("rol")
-
-            user = Personal.query.filter_by(username=username, rol=rol).first()
-
-            if user and user.contrasena == password: 
-                return jsonify({
-                    "success": True,
-                    "message": f"Bienvenido al sistema, {user.nombre}",
-                    "data": {
-                    "id_personal": user.id_personal,
-                    "nombre": user.nombre,
-                    "rol": user.rol
-                    }
-                }), 200
-        
-            return jsonify({
-            "success": False, 
-            "message": "Usuario, contraseña o rol incorrectos"
-            }), 401
-
-        except Exception as e:
-            return jsonify({"success": False, "message": str(e)}), 500
 
     @app.route("/api/health")
     def health():
-        return jsonify({"status": "ok", "service": "Obras Públicas — API v2.0"}), 200
+        return jsonify({"status": "ok", "service": "API Obras Públicas"}), 200
 
     @app.errorhandler(404)
     def h404(e): return jsonify({"success":False,"message":"Ruta no encontrada."}), 404
-    
-    @app.errorhandler(405)
-    def h405(e): return jsonify({"success":False,"message":"Método HTTP no permitido."}), 405
-    
-    @app.errorhandler(500)
-    def h500(e): return jsonify({"success":False,"message":"Error interno del servidor."}), 500
 
     return app 
