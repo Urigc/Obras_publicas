a00155ecbaf87cbb35a3b7145bb70484ca280265
Uriel Gonzalez <gonzalezcasianouriel@gmail.com>
2026-05-02 03:24:07 -0600

    Add files via upload

---
+++ backend/app/models.py
@@ -0,0 +1,22 @@
+from database import db
+
+class Personal(db.Model):
+    __tablename__ = 'personal'
+    __table_args__ = {'schema': 'public'} # Especificamos el esquema según tu captura[cite: 2]
+
+    # Definimos las columnas exactamente como están en tu base de datos
+    codigo_personal = db.Column(db.String, primary_key=True)
+    nombre          = db.Column(db.String, nullable=False)
+    apellido_paterno = db.Column(db.String, nullable=False)
+    apellido_materno = db.Column(db.String, nullable=True) # Es anulable según tu imagen[cite: 2]
+    
+    username        = db.Column(db.String, unique=True, nullable=False)
+    password_hash   = db.Column(db.Text, nullable=False)
+
+    def to_dict(self):
+        """Convierte el objeto a un diccionario para enviarlo como JSON al frontend."""
+        return {
+            "id": self.codigo_personal,
+            "nombre": f"{self.nombre} {self.apellido_paterno}",
+            "username": self.username
+        }
