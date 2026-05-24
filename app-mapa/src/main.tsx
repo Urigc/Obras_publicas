b76dc56f0915c970629146af3c15abe9fa533fee
Uriel Gonzalez <gonzalezcasianouriel@gmail.com>
2026-05-24 11:27:17 -0600

    Update main.tsx

--- app-mapa/src/main.tsx
+++ app-mapa/src/main.tsx
@@ -1,5 +1,7 @@
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import 'leaflet/dist/leaflet.css'                      // ← Línea nueva
+import '@changey/react-leaflet-markercluster/dist/styles.min.css'  // ← Línea nueva
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(<App />)
