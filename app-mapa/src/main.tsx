0c50f495c9b711c5827ae075fe27b17cc5f39399
Uriel Gonzalez <gonzalezcasianouriel@gmail.com>
2026-05-24 23:43:04 -0600

    Update main.tsx

--- app-mapa/src/main.tsx
+++ app-mapa/src/main.tsx
@@ -1,8 +1,6 @@
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import 'leaflet/dist/leaflet.css'
-import 'react-leaflet-cluster/lib/assets/MarkerCluster.css'
-import 'react-leaflet-cluster/lib/assets/MarkerCluster.Default.css'
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(<App />)
