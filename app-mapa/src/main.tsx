9b866b6382727953756054a7a652f0f5c76c698f
Uriel Gonzalez <gonzalezcasianouriel@gmail.com>
2026-05-24 22:21:33 -0600

    Update main.tsx

--- app-mapa/src/main.tsx
+++ app-mapa/src/main.tsx
@@ -1,7 +1,8 @@
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import 'leaflet/dist/leaflet.css'                       // ✅ Importante
-import '@changey/react-leaflet-markercluster/dist/styles.min.css'  // ✅ Importante
+import 'leaflet/dist/leaflet.css'
+import 'react-leaflet-cluster/lib/assets/MarkerCluster.css'
+import 'react-leaflet-cluster/lib/assets/MarkerCluster.Default.css'
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(<App />)
